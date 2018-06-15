9220580b75b1b74f1bec7e29fc3c400f1b55c1bd
Valentin Py <valentin.py@gmail.com>
2018-06-15 15:20:08 +0200

    add serial print in test user code

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -1,10 +1,34 @@
 #include "Copter.h"
 
 #ifdef USERHOOK_INIT
+
+static void setup_uart(AP_HAL::UARTDriver *uart, const char *name)
+{
+    if (uart == nullptr) {
+        // that UART doesn't exist on this platform
+        return;
+    }
+    uart->begin(57600);
+}
+
+static void test_uart(AP_HAL::UARTDriver *uart, const char *name, int counter)
+{
+    if (uart == nullptr) {
+        // that UART doesn't exist on this platform
+        return;
+    }
+    uart->printf("[VPY] Hello on UART %s at %.3f seconds, counter: %d\n",
+                 name, (double)(AP_HAL::millis() * 0.001f), counter);
+}
+
+
 void Copter::userhook_init()
 {
     // put your initialisation code here
     // this will be called once at start-up
+    hal.scheduler->delay(1000); //Ensure that the uartA can be initialized
+    setup_uart(hal.uartD, "uartD");  // telemetry 2
+    setup_uart(hal.uartE, "uartE"); // 2nd GPS
 }
 #endif
 
@@ -40,5 +64,12 @@ void Copter::userhook_SlowLoop()
 void Copter::userhook_SuperSlowLoop()
 {
     // put your 1Hz code here
+    // static int vpy_test_var_static = 0;
+    // vpy_test_var_static ++;
+
+    vpy_test_var ++;
+
+    // test_uart(hal.uartD, "uartD", vpy_test_var);
+    test_uart(hal.uartE, "uartE", vpy_test_var);
 }
 #endif
--- ArduCopter/UserVariables.h
+++ ArduCopter/UserVariables.h
@@ -4,13 +4,15 @@
 // variables
 #ifdef USERHOOK_VARIABLES
 
-#if WII_CAMERA == 1
-WiiCamera           ircam;
-int                 WiiRange=0;
-int                 WiiRotation=0;
-int                 WiiDisplacementX=0;
-int                 WiiDisplacementY=0;
-#endif  // WII_CAMERA
+// #if WII_CAMERA == 1
+// WiiCamera           ircam;
+// int                 WiiRange=0;
+// int                 WiiRotation=0;
+// int                 WiiDisplacementX=0;
+// int                 WiiDisplacementY=0;
+// #endif  // WII_CAMERA
+
+int vpy_test_var = 0;
 
 #endif  // USERHOOK_VARIABLES
 
