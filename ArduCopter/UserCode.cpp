b418037a9106a460743786c0f72502513cac5029
Valentin Py <valentin.py@gmail.com>
2018-06-18 18:26:48 +0200

    send new mavlink message

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -2,33 +2,23 @@
 
 #ifdef USERHOOK_INIT
 
-static void setup_uart(AP_HAL::UARTDriver *uart, const char *name)
-{
-    if (uart == nullptr) {
-        // that UART doesn't exist on this platform
-        return;
-    }
-    uart->begin(57600);
-}
-
-static void test_uart(AP_HAL::UARTDriver *uart, const char *name, int counter)
-{
-    if (uart == nullptr) {
-        // that UART doesn't exist on this platform
-        return;
-    }
-    uart->printf("[VPY] Hello on UART %s at %.3f seconds, counter: %d\n",
-                 name, (double)(AP_HAL::millis() * 0.001f), counter);
-}
-
-
 void Copter::userhook_init()
 {
     // put your initialisation code here
     // this will be called once at start-up
     hal.scheduler->delay(1000); //Ensure that the uartA can be initialized
-    setup_uart(hal.uartD, "uartD");  // telemetry 2
-    setup_uart(hal.uartE, "uartE"); // 2nd GPS
+
+    if (hal.uartE == nullptr) {
+        // that UART doesn't exist on this platform
+        return;
+    }
+    hal.uartE->begin(57600);
+
+    hal.uartE->printf("\n\n[Serial VPY] Setup finished!\n\n");
+    hal.uartE->printf("------------------------------------------------------------\n");
+    hal.uartE->printf("---------------- SERGE EST FORT!! --------------------------\n");
+    hal.uartE->printf("------------------------------------------------------------\n");
+
 }
 #endif
 
@@ -50,6 +40,24 @@ void Copter::userhook_50Hz()
 void Copter::userhook_MediumLoop()
 {
     // put your 10Hz code here
+    // vpy_test_var ++;
+
+    uint32_t data_length = hal.uartE->available();
+
+    static int i = 0;
+    static char buffer[100]="\0";
+
+    // read char by char
+    while(hal.uartE->available() > 0){
+        buffer[i] = hal.uartE->read();
+        i++;
+    }
+
+    // if last char was and end of line, print
+    if (buffer[i-1]== '\n' || buffer[i-1]== '\r'){
+        i = 0;
+        hal.uartE->printf("[VPY] at %.3f seconds, data_length: %d, data: %s\n", (double)(AP_HAL::millis() * 0.001f), data_length, buffer);
+    }
 }
 #endif
 
@@ -57,19 +65,30 @@ void Copter::userhook_MediumLoop()
 void Copter::userhook_SlowLoop()
 {
     // put your 3.3Hz code here
+    static int16_t vpy_test_var_static = 0;
+    vpy_test_var_static ++;
+  
+    uint8_t chan_mask = GCS_MAVLINK::active_channel_mask();
+    for (uint8_t i=0; i<MAVLINK_COMM_NUM_BUFFERS; i++) {
+        if ((chan_mask & (1U<<i)) == 0) {
+            // not active
+            continue;
+        }
+        mavlink_channel_t chan = (mavlink_channel_t)i;
+        if (HAVE_PAYLOAD_SPACE(chan, BUTTON_CHANGE)) {
+            mavlink_msg_vpy_test_send(chan, vpy_test_var_static);
+            // mavlink_msg_power_status_send(chan, myVar1, myVar2, myVar3);
+
+            hal.uartE->printf("[VPY] Mavlink message sent: 'VPY_TEST: {vpy_test=%d}'\n", vpy_test_var_static);
+        }
+    }
 }
 #endif
 
 #ifdef USERHOOK_SUPERSLOWLOOP
 void Copter::userhook_SuperSlowLoop()
 {
-    // put your 1Hz code here
-    // static int vpy_test_var_static = 0;
-    // vpy_test_var_static ++;
-
-    vpy_test_var ++;
-
-    // test_uart(hal.uartD, "uartD", vpy_test_var);
-    test_uart(hal.uartE, "uartE", vpy_test_var);
+    // put your 1Hz code here   
+    
 }
 #endif
